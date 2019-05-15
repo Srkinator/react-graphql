99930a1af0d0bcd303fe618fa919f759d78377a3
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2019-05-15 23:22:33 +0200

    Setup header and navigation bar

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Link from 'next/link'
+
+
+const Home = props => {
+  return <div>
+    <Link href="/sell">
+      <a></a>
+    </Link>
+  </div>
+}
+
+export default Home
---
+++ frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Link from 'next/link'
+
+const Sell = props => {
+  return <div>
+    sell
+    <Link href="/">
+      <a>go to home</a>
+    </Link>
+  </div>
+}
+
+export default Sell
