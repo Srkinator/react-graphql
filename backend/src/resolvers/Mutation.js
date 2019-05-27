98f161665ecd55e391ba225984fe2db629ba7a73
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2019-05-27 22:33:38 +0200

    Update Item with Queries and Mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -6,6 +6,17 @@ const mutations = {
       }
     }, info)
     return item
+  },
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args }
+    delete updates.id
+
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, info)
   }
 };
 
--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b0eaad3fc3de8090c889";
+/******/ 	var hotCurrentHash = "62ff26dfd9d515369dff";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
