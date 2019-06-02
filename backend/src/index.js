ca4e1bdf900acfddcb7234e4b0c9860010f6ee94
Srdjan Popovic <srdjan.popovic@guidefitter.com>
2019-06-02 22:47:44 +0200

    Display currently logged in user in NavBar using middleware and render props

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,33 @@
-require('dotenv').config({ path: 'variables.env' })
-const cookieParser = require('cookie-parser')
-const createServer = require('./createServer')
-const db = require('./db')
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
-const server = createServer()
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
 
-server.express.use(cookieParser())
+const server = createServer();
 
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL
+server.express.use(cookieParser());
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
   }
-}, data => {
-  console.log(`Server is now running on port http:/localhost:${data.port}`)
-})
+  next();
+});
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5b5ebee800e74c78f710";
+/******/ 	var hotCurrentHash = "ccce2fed37b888d8bcb4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
